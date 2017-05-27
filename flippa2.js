938e723bf86eb4e63d9c096d9b9eee11c98190d8
Jarett Dunn <jarett+github@dcssquared.com>
2017-05-27 14:34:09 -0300

    initial

---
+++ flippa2.js
@@ -0,0 +1,85 @@
+var Flippa = require('./node_modules/flippa/dist/Flippa.js');
+var request = require('request');
+var revenues = [];
+	var profits = [];
+	var bins = [];
+	var htmls = [];
+	var ratios = {};
+abc = 0;
+	flippa = new Flippa();
+function sortNumber(a,b) {
+   return a - b;
+}
+	function getresponse(result, n){
+	//console.log(n);
+	for (var i = 0; i < result.length + 0; i++) { 
+		//console.log(result[i]);
+		revenues[i + n] = result[i]['revenue_per_month'];
+		//console.log(revenues[i + n]);
+		profits[i + n] = result[i]['profit_per_month'];
+		bins[i + n] = result[i]['current_price'];//current_pricebuy_it_now_price
+		htmls[i + n] = result[i]['html_url'];
+	}
+	var i = n;
+	for (var bin in bins) {
+		//console.log(i);
+		if (bin != undefined){
+		//console.log(bins[i]);
+		ratios[htmls[i]] = {'bins': parseFloat(bins[i]), 'revenues': parseFloat(revenues[i])};
+		//console.log(ratios[i]);
+		}		
+		i++;
+	}
+	i = n;
+		
+	}
+	
+	flippa
+  .authenticate({
+    grant_type: "password",
+    username: "yourflippaemail",
+    password: "yourflippapassword"
+  })
+  .then(function(response) {
+    // Authentication succeeded; can now make authorized requests.
+   // console.log(flippa.client.accessToken);
+	flippa
+  .listings
+  .list({filter: {status: "open", has_verified_revenue: true}})//,has_bin: true}})
+  .then(function lala (response) {
+    //console.log(response.body.data[0]);
+	if (response == undefined){
+	
+		for (var key in ratios){
+		//console.log(key['revenues']);
+		if (key != undefined && ratios[key]['revenues'] != undefined && (ratios[key]['bins'] / ratios[key]['revenues']) != Infinity && ratios[key]['revenues'] >= 1000){
+		console.log((ratios[key]['bins'] / ratios[key]['revenues']) + ' months for ' + key + ' earning $' + ratios[key]['revenues'] + ' at $' + ratios[key]['bins']) ;
+		}
+	}
+	
+	}
+	else{
+		
+	if (abc >= 1){
+	//console.log(response.body);
+	var result2 = (JSON.parse(response.body));
+	var result = result2.data;
+			getresponse(result,((result2.meta)['page_number'] - 1) * (result2.meta)['page_size'] );
+			request((result2.links)['next'], function (error, response, body) {
+				lala(response);
+			
+		}) 	
+	}
+	else {
+		var result = (response.body.data);
+			getresponse(result,(response.body.meta['page_number'] - 1) * response.body.meta['page_size'] );
+			request(response.body.links.next, function (error, response, body) {
+				lala(response);
+			
+		});	 	
+	}
+	abc++;
+	}
+  })
+  })
+ 
