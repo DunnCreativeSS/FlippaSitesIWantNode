b92ae3b484be521951b02c78a21858f1527535e1
Jarett Dunn <jarett+github@dcssquared.com>
2017-05-27 19:08:24 -0300

    BIN variable

--- flippa2.js
+++ flippa2.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res) {
 	ratios = {};
 	abc = 0;
 		if (!req.param('minrevenue')){
-		lala2 = "<html><meta></meta><body><form action='/' method='GET'>Maximum months ROI:<input type='text' value='24' name='maxroi'> Minimum revenue:<input type='text' value='1000' name='minrevenue'><input type='submit'></form>";
+		lala2 = "<html><meta></meta><body><form action='/' method='GET'>Is BIN:<select name='has_bin'><option value='Y'>Yes</option><option value='N'>No</option></select> Maximum months ROI:<input type='text' value='24' name='maxroi'> Minimum revenue:<input type='text' value='1000' name='minrevenue'><input type='submit'></form>";
                         
 		lala2+= "</body></html>";
 		res.send(lala2);
@@ -30,8 +30,8 @@ app.get('/', function(req, res) {
 		flippa
 			.authenticate({
 				grant_type: "password",
-				username: "jarettrsdunn@gmail.com",
-				password: "wordpass"
+				username: "yourflippaemail",
+				password: "yourflippapassword"
 			})
 			.then(function(response) {
 				// Authentication succeeded; can now make authorized requests.
@@ -39,15 +39,18 @@ app.get('/', function(req, res) {
 				flippa
 					.listings
 					.list({
-						filter: {
-							status: "open",
-							has_verified_revenue: true
-						}
+							filter: {
+								status: "open",
+								has_verified_revenue: true,
+								has_bin: (req.param('has_bin') == "Y") ? 'true' : 'false' 
+							}
+						
 					}) //,has_bin: true}})
 					.then(function lala(response) {
 						//console.log(response.body.data[0]);
 						if (response == undefined) {
-							lala2 = "<html><meta></meta><body><form action='/' method='GET'>Maximum months ROI:<input type='text' value='" + req.param('maxroi') + "' name='maxroi'> Minimum revenue:<input type='text' value='" + req.param('minrevenue') + "' name='minrevenue'><input type='submit'></form>";
+							(req.param('has_bin') == "Y") ? lala2 = "<html><meta></meta><body><form action='/' method='GET'>Is BIN:<select name='has_bin'><option value='Y' selected>Yes</option><option value='N'>No</option></select> Maximum months ROI:<input type='text' value='" + req.param('maxroi') + "' name='maxroi'> Minimum revenue:<input type='text' value='" + req.param('minrevenue') + "' name='minrevenue'><input type='submit'></form>" : lala2 = "<html><meta></meta><body><form action='/' method='GET'>Is BIN:<select name='has_bin'><option value='Y'>Yes</option><option value='N' selected>No</option></select> Maximum months ROI:<input type='text' value='" + req.param('maxroi') + "' name='maxroi'> Minimum revenue:<input type='text' value='" + req.param('minrevenue') + "' name='minrevenue'><input type='submit'></form>";
+							
 							for (var key in ratios) {
 								//console.log(key['revenues']);
 								if ((ratios[key]['bins'] / ratios[key]['revenues']) <= req.param('maxroi') && key != undefined && ratios[key]['revenues'] != undefined && (ratios[key]['bins'] / ratios[key]['revenues']) != Infinity && ratios[key]['revenues'] >= req.param('minrevenue')) {
@@ -64,14 +67,14 @@ app.get('/', function(req, res) {
 								//console.log(response.body);
 								var result2 = (JSON.parse(response.body));
 								var result = result2.data;
-								getresponse(result, ((result2.meta)['page_number'] - 1) * (result2.meta)['page_size']);
+								getresponse(result, req.param('has_bin'), ((result2.meta)['page_number'] - 1) * (result2.meta)['page_size']);
 								request((result2.links)['next'], function(error, response, body) {
 									lala(response);
 
 								})
 							} else {
 								var result = (response.body.data);
-								getresponse(result, (response.body.meta['page_number'] - 1) * response.body.meta['page_size']);
+								getresponse(result, req.param('has_bin'), (response.body.meta['page_number'] - 1) * response.body.meta['page_size']);
 								request(response.body.links.next, function(error, response, body) {
 									lala(response);
 
@@ -89,15 +92,21 @@ app.listen(3000, function() {
 })
 
 
-function getresponse(result, n) {
+function getresponse(result, has_bin, n){
     //console.log(n);
     for (var i = 0; i < result.length + 0; i++) {
         //console.log(result[i]);
         revenues[i + n] = result[i]['revenue_per_month'];
         //console.log(revenues[i + n]);
         profits[i + n] = result[i]['profit_per_month'];
-        bins[i + n] = result[i]['current_price']; //current_pricebuy_it_now_price
-        htmls[i + n] = result[i]['html_url'];
+		if (has_bin == "N"){
+			bins[i + n] = result[i]['current_price']; //current_pricebuy_it_now_price
+        }
+		else {
+			bins[i + n] = result[i]['buy_it_now_price']; //current_pricebuy_it_now_price
+
+		}
+		htmls[i + n] = result[i]['html_url'];
     }
     var i = n;
     for (var bin in bins) {
